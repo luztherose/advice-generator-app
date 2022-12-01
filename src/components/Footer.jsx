5c913f4a8f2658af1361161db9de597088fa8275
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2022-11-30 21:08:13 -0500

    Inital commit :rocket:

---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <footer className="attribution flex justify-center pb-8 md:flex-col md:items-center">
+      <p className="mr-2">
+        Challenge by{" "}
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        .
+      </p>
+      <p>
+        Coded by{" "}
+        <a href="https://luztherose.github.io/portfolio/">Luz De La Rosa</a>.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
